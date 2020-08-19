342ed8521445be5f8f622441ad4e1bb01efb228b
rawanfatima <rawanfatima@outlook.com>
2020-08-18 20:17:24 -0700

    Updates

---
+++ precache-manifest.c5ead491a5aaedb39dd1d1d37efa71d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7bd72bfdab7cc9e81a1452e3bfa2eb6",
+    "url": "//waveess.github.io/redux/index.html"
+  },
+  {
+    "revision": "c43803e14c7be57e3b74",
+    "url": "//waveess.github.io/redux/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "a482ad502074fcd41e08",
+    "url": "//waveess.github.io/redux/static/js/2.1a90d4a1.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "//waveess.github.io/redux/static/js/2.1a90d4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43803e14c7be57e3b74",
+    "url": "//waveess.github.io/redux/static/js/main.4d0f9465.chunk.js"
+  },
+  {
+    "revision": "5c45e766816c8572dc57",
+    "url": "//waveess.github.io/redux/static/js/runtime-main.750ed694.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "//waveess.github.io/redux/static/media/spinner.ab497855.gif"
+  }
+]);
